99e360bfed1b092244cb02880b99a5725dfe5cee
CeceliaCD <cecelia.chollettedickson@rutgers.edu>
2021-02-04 20:56:54 -0500

    Still need to work on checking for a wall or fire. Also haven't finished their random generation. Will work on later.

--- Maze.java
+++ Maze.java
@@ -1,38 +1,135 @@
-import java.awt.Color;
-import java.awt.Graphics;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.lang.Math;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Cell extends Observable{
+	int x;
+	int y;
+	boolean[] dir = new boolean[4];//directions up, down, left and right
+	boolean visited;
+
+	private final Rectangle rectangle;
+	private final int cellSpace = 15;
+
+	public enum Type{
+		EMPTY,
+		AGENTPATH,
+		START,
+		GOAL,
+		WALL,
+		FIRE;
+	}
+
+	public Cell(int x, int y, int cellSpace){
+		this.x = x;
+		this.y = y;
+		//this.cellSpace = cellSpace;
+		this.type = Type.EMPTY;
+
+		private final Map<Type, Color> typeMap;
+		
+		dir[0] = false;
+		dir[1] = false;
+		dir[2] = false;
+		dir[3] = false;
+
+		typeMap = new HashMap();
+		typeMap.put(Type.EMPTY, Color.WHITE);
+		typeMap.put(Type.WALL, Color.BLACK);
+		typeMap.put(Type.AGENTPATH, Color.GREEN);
+		typeMap.put(Type.START, Color.BLUE);
+		typeMap.put(Type.GOAL, Color.PURPLE);
+		typeMap.put(Type.FIRE, Color.RED);
+
+	}
+
+	public void visit(){
+		visited = true;
+	}
+
+	public Type getType(){
+		return this.type;
+	}
+
+	public boolean isWall(){
+		return (this.type == type.WALL);
+	}
+
+}
+
+
 
 public class Maze extends JFrame {
+
 	
-	final int DIM = 10;
-	int[][] myMaze = new int[DIM][DIM];
+	final int DIM = 50;
+	Cell[][] myMaze= new Cell[][]();
+	Cell start;
+	Cell goal;
+	int counter = 0;
 	
 	Random rand = new Random();
 	
     
 	
-	public Maze() {
+	public Maze(int dimension, int cellsize) {
 		setTitle("Maze");
 		setSize(640, 480);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.DIM = dimension;
+		this.myMaze = new Cell[dimension][dimension];
+		Random rand = new Random();
+
+		for (int row = 0; row < myMaze.length; row++) {
+           		for (int col = 0; col < myMaze[row].length; col++) {
+               			Cell cell = new Cell(col, row, cellsize);
+						   cell.addObserver(this);
+						   myMaze[col][row] = cell;
+            }
+        }
 		
 	}
 	
-	public void populate(int [][] myMaze) {
-		for (int row = 0; row < myMaze.length; row++) {
-           		for (int col = 0; col < myMaze[row].length; col++) {
-               			if (rand.nextDouble() < 0.5) {
-                    			myMaze[row][col] = 1;
-               		      } else {
-                   			myMaze[row][col] = 0;
-                }
-            }
-        }
+	public void generateMaze(Cell currCell) {
+		Random rand = new Random();
+		currCell.visit();
+		count++;
+
+
+	}
+
+//advancingFire
+
+//checkNeighbor
+public Cell checkNeighbor(){
+	
 }
+
+ public void DFS(){
+
+  }
+
+  public void BFS(){
+
+  }
+
+  public void AStar(){
+
+  }
+
+
+
 	
 	
   @Override
@@ -55,6 +152,8 @@ public class Maze extends JFrame {
 		  }
 	  }
   }
+
+ 
   		public static void main(String[] args) {
   			SwingUtilities.invokeLater(new Runnable() {
   				
@@ -66,4 +165,6 @@ public class Maze extends JFrame {
   				
   			});
   		}
+
+
 }
